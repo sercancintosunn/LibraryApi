95af442d2d029fada08c47154b4035e401c02123
Sercan Çintosun <sercancintosun000@gmail.com>
2026-07-23 02:07:49 +0300

    Category Controller created

---
+++ Library.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using LibraryApi.Business.Interfaces.Services;
+using LibraryApi.Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IGenericService<Category> _categoryService;
+
+        public CategoryController(IGenericService<Category> categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories =  await _categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            if(category == null)
+            {
+                return NotFound(new { message = "Category bulunamadı" });
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]Category category)
+        {
+            var created = await _categoryService.CreateAsync(category);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+    }
+}
