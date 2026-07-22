5ddd09c28a280ca0dfc4d79570204c8dd3c39707
Sercan Çintosun <sercancintosun000@gmail.com>
2026-07-22 15:35:24 +0300

    configure dependency ınjection for repositories

--- Library.WebApi/Program.cs
+++ Library.WebApi/Program.cs
@@ -1,4 +1,8 @@
+using LibraryApi.Business.Interfaces;
+using LibraryApi.Business.Interfaces.Repositories;
+using LibraryApi.DataAccess;
 using LibraryApi.DataAccess.Data;
+using LibraryApi.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<ILoanRepository,LoanRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
