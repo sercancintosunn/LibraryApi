19bfcb64884df0a3a343072b83709c91c2252f02
Sercan Çintosun <sercancintosun000@gmail.com>
2026-07-23 15:46:17 +0300

    feat(Business): Loans Dto created

---
+++ LibraryApi.Business/DTOs/Loans/CreateLoanDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Business.DTOs.Loans
+{
+    public class CreateLoanDto
+    {
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+    }
+}
---
+++ LibraryApi.Business/DTOs/Loans/LoanResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Business.DTOs.Loans
+{
+    public class LoanResponseDto
+    {
+        public int Id { get; set; }
+        public string BookTitle { get; set; } = string.Empty;
+        public string MemberName { get; set; } = string.Empty;
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
