d4d0639a554135dfc46b39dedefe77d26c3fb7fa
Sercan Çintosun <sercancintosun000@gmail.com>
2026-08-03 13:21:37 +0300

    Members dto created

---
+++ LibraryApi.Business/DTOs/Members/AuthResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Business.DTOs.Members
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public MemberResponseDto Member { get; set; } = null!;
+    }
+}
